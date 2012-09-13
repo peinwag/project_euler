9d61b8602f8883505f882189132e3177c8b7c584
Patrick Einwag <pe@rakuten.de>
2012-09-13 08:40:41 +0200

    p6 solved

--- p006/p006.php
+++ p006/p006.php
@@ -1,1 +1,13 @@
 <?php
+$sumOfSquares = 0;
+$squareOfTheSum = 0;
+
+for($i = 1; $i <= 100; $i++) {
+
+	$sumOfSquares = bcadd($sumOfSquares, bcpow($i, 2));
+	$squareOfTheSum = bcadd($squareOfTheSum, $i);
+}
+
+$squareOfTheSum = bcpow($squareOfTheSum, 2);
+
+echo bcsub($squareOfTheSum, $sumOfSquares);
