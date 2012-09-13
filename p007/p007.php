31e0c62983ce0310b58b55a888832ae0c12047f7
Patrick Einwag <pe@rakuten.de>
2012-09-13 10:42:53 +0200

    p7 solved

--- p007/p007.php
+++ p007/p007.php
@@ -3,3 +3,12 @@
 define('LIBRARY_MODE', 1);
 
 require_once '../p003/p003.php';
+
+$start = 2;
+for($i = 2; $i <= 10001; $i++) {
+
+    $prime = getNextPrime($start);
+    $start = $prime;
+}
+
+echo $prime;
